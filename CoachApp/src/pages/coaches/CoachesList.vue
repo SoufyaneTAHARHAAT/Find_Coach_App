fcca81693cf01c68ae0bdddc4e262f382749dc80
SoufyaneTAHARHAAT <soufyanetahar@gmail.com>
2024-09-24 00:38:47 +0200

    Added the CoachItem component and used it in the CoachesList component

--- CoachApp/src/pages/coaches/CoachesList.vue
+++ CoachApp/src/pages/coaches/CoachesList.vue
@@ -1,47 +1,63 @@
 <template>
-    <section>
-        Filter
-    </section>
-    <section>
-      <div class="controls">
-        <button>Refresh</button>
-        <router-link to="/register">Register as coach</router-link>
-      </div>
-        Coaches List
-        <ul v-if="hasCoaches">
-          <li v-for="coach in filteredCoaches" :key="coach.id">
-            {{ coach.firstName }}
-          </li>
-        </ul>
-        <h3 v-else>No coaches to load !</h3>
-    </section>
+  <section>Filter</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as coach</router-link>
+    </div>
+    Coaches List
+    <ul v-if="hasCoaches">
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :hourlyRate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
+    </ul>
+    <h3 v-else>No coaches to load !</h3>
+  </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 export default {
+  components: { CoachItem },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches'];
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
 * {
   box-sizing: border-box;
 }
 
 html {
-  font-family: "Roboto", sans-serif;
+  font-family: 'Roboto', sans-serif;
 }
 
 body {
   margin: 0;
+}
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
