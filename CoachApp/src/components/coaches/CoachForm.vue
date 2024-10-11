6030bcafaf906dd0f287dc7c9a6310933fce00aa
SoufyaneTAHARHAAT <soufyanetahar@gmail.com>
2024-10-11 23:54:01 +0200

    form validation

--- CoachApp/src/components/coaches/CoachForm.vue
+++ CoachApp/src/components/coaches/CoachForm.vue
@@ -1,36 +1,42 @@
 <template>
     <form @submit.prevent="submitData">
-        <div class="form-control">
+        <div class="form-control" :class="{ invalid: !this.firstname.isValid}">
             <label for="firstname">Firstname</label>
-            <input type="text" id="firstname" v-model.trim="firstname">
+            <input type="text" id="firstname" v-model.trim="firstname.val">
+            <p v-if="!this.firstname.isValid">Please enter a firstname.</p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{ invalid: !this.lastname.isValid}">
             <label for="lasttname">Lastname</label>
-            <input type="text" id="lastname" v-model.trim="lastname">
+            <input type="text" id="lastname" v-model.trim="lastname.val">
+            <p v-if="!this.lastname.isValid">Please enter a lastname.</p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{ invalid: !this.description.isValid}">
             <label for="description">Description</label>
-            <textarea id="description" rows="10" v-model.trim="description"></textarea>
-        </div>
-        <div class="form-control">
+            <textarea id="description" rows="10" v-model.trim="description.val"></textarea>
+            <p v-if="!this.description.isValid">Please enter a description.</p>
+          </div>
+        <div class="form-control" :class="{ invalid: !this.rate.isValid}">
             <label for="rate">Hourly Rate</label>
-            <input type="number" id="rate" value="5" v-model.number="rate">
+            <input type="number" id="rate" value="5" v-model.number="rate.val">
+            <p v-if="!this.rate.isValid">Please a valid rate.</p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{ invalid: !this.areas.isValid}">
             <h3>Areas Of Expertise</h3>
             <div>
-                <input type="checkbox" id="frontend" value="frontend" v-model="areas">
+                <input type="checkbox" id="frontend" value="frontend" v-model="areas.val">
                 <label for="frontend">Frontend Development</label>
             </div>
             <div>
-                <input type="checkbox" id="backend" value="backend" v-model="areas">
+                <input type="checkbox" id="backend" value="backend" v-model="areas.val">
                 <label for="backend">Backend Development</label>
             </div>
             <div>
-                <input type="checkbox" id="career" value="career" v-model="areas">
+                <input type="checkbox" id="career" value="career" v-model="areas.val">
                 <label for="career">Career Advisory</label>
             </div>
+            <p v-if="!this.areas.isValid">At least one expertise must be selected.</p>
         </div> 
+        <p>Please fix the above errors then submit again.</p>
         <base-button>Register</base-button>
     </form>
 </template>
@@ -39,11 +45,27 @@
 export default {
     data() {
         return {
-            firstname: '',
-            lastname: '',
-            description: '',
-            rate: null,
-            areas: [],
+            firstname: {
+              val: '',
+              isValid: true,
+            },
+            lastname: {
+              val: '',
+              isValid: true,
+            },
+            description: {
+              val: '',
+              isValid: true,
+            },
+            rate: {
+              val: null,
+              isValid: true,
+            },
+            areas: {
+              val: [],
+              isValid: true,
+            },
+            formIsValid: true,
         };
     },
     computed: {
@@ -51,13 +73,40 @@ export default {
       return this.$store.getters["coaches/isCoach"];
     }},
     methods: {
+      validateForm() {
+        this.formIsValid= true;
+        if(this.firstname.val==='') {
+          this.firstname.isValid=false;
+          this.formIsValid=false;
+        }
+        if(this.lastname.val==='') {
+          this.lastname.isValid=false;
+          this.formIsValid=false;
+        }
+        if(this.description.val==='') {
+          this.description.isValid=false;
+          this.formIsValid=false;
+        }
+        if(!this.rate.val || this.rate.val < 0) {
+          this.rate.isValid=false;
+          this.formIsValid=false;
+        }
+        if(this.areas.val.length===0) {
+          this.firstname.isValid=false;
+          this.formIsValid=false;
+        }
+      },
         submitData() {
+        this.validateForm();
+        if(!this.isValid) {
+          return;
+        }
             const submittedData = {
-                first: this.firstname,
-                last: this.lastname,
-                desc: this.description,
-                rate: this.rate,
-                areas: this.areas
+                first: this.firstname.val,
+                last: this.lastname.val,
+                desc: this.description.val,
+                rate: this.rate.val,
+                areas: this.areas.val,
             }
             this.$emit("save-data", submittedData);
             console.log(this.isCoach);
--- CoachApp/src/store/modules/coaches/actions.js
+++ CoachApp/src/store/modules/coaches/actions.js
@@ -10,4 +10,5 @@ export default {
         };
         context.commit("registerCoach", data);
     },
+    
 };
