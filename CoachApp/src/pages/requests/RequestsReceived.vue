1a5487660e34cfae8b7455ecc2b4fedeec16ed9e
SoufyaneTAHARHAAT <soufyanetahar@gmail.com>
2024-10-18 00:52:17 +0200

    Made the requests components and module

--- CoachApp/src/pages/requests/RequestsReceived.vue
+++ CoachApp/src/pages/requests/RequestsReceived.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>This would be the requests list component</h1>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item v-for="req in receivedRequests"
+        :key="req.id"
+        :email="req.email"
+        :message="req.message">
+        </request-item>
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import requestItem from '../../components/requests/requestItem.vue';
+
+export default {
+    components: {requestItem},
+
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+        return this.$store.getters['requests/hasRequests'];
+    }
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
