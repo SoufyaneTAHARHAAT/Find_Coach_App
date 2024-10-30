53003d65c3ee89973b50f1c3ab0d25d66ea3786c
SoufyaneTAHARHAAT <soufyanetahar@gmail.com>
2024-10-30 23:52:28 +0100

    Sending PUT request to Firebase database

--- CoachApp/src/store/modules/coaches/actions.js
+++ CoachApp/src/store/modules/coaches/actions.js
@@ -2,22 +2,32 @@ import axios from "axios";
 
 export default {
     async registerCoach(context, payload) {
+        const userId = context.rootGetters.userId;
         const data = {
-            id: context.rootGetters.userId,
             firstName: payload.first,
             lastName: payload.last,
             areas: payload.areas,
             description: payload.desc,
             hourlyRate: payload.rate,
         };
-        context.commit("registerCoach", data);
-        
         try {
-            await axios.post('http://localhost:3000/coaches', data);
+            await axios.put(`https://find-coach-app-38f67-default-rtdb.firebaseio.com/coaches/${userId}.json`, {body: data});
         }
         catch(error) {
-            console.error("Failed to send data", error);
+            console.error("Error sending data to database", error);
         }
+
+        context.commit("registerCoach", {
+            id: userId,
+            ...data,
+        });
+        
+        // try {
+        //     await axios.post('http://localhost:3000/coaches', data);
+        // }
+        // catch(error) {
+        //     console.error("Failed to send data", error);
+        // }
     },
     
 };
--- Find_app_coach_backend/index.js
+++ Find_app_coach_backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send("Hello world!");
+    res.json(coaches);
 });
 
 app.post('/coaches', (req, res) => {
