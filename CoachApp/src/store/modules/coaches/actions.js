fb90b70ae8733eac742cabbe1a8f1470a18e1726
SoufyaneTAHARHAAT <soufyanetahar@gmail.com>
2024-11-08 01:09:50 +0100

    changed  the fetching format

--- CoachApp/src/store/modules/coaches/actions.js
+++ CoachApp/src/store/modules/coaches/actions.js
@@ -11,7 +11,7 @@ export default {
             hourlyRate: payload.rate,
         };
         try {
-            await axios.put(`https://find-coach-app-38f67-default-rtdb.firebaseio.com/coaches/${userId}.json`, {body: data});
+            await axios.put(`https://find-coach-app-38f67-default-rtdb.firebaseio.com/coaches/${userId}.json`, {data});
         }
         catch(error) {
             console.error("Error sending data to database", error);
@@ -28,14 +28,14 @@ export default {
             const response = await axios.get('https://find-coach-app-38f67-default-rtdb.firebaseio.com/coaches.json');
             const coachesLoaded = [];
             for (const id in response.data) {
-                console.log(response.data[id].firstName);
+                //console.log(response.data[id].firstName);
                 const loadedCoach = {
                     id: id,
-                    firstName: response.data[id].body.firstName || '',
-                    lastName: response.data[id].body.lastName || '',
-                    areas: response.data[id].body.areas || [],
-                    description: response.data[id].body.description || '',
-                    hourlyRate: response.data[id].body.hourlyRate || 0,
+                    firstName: response.data[id].firstName || '',
+                    lastName: response.data[id].lastName || '',
+                    areas: response.data[id].areas || [],
+                    description: response.data[id].description || '',
+                    hourlyRate: response.data[id].hourlyRate || 0,
                 }
                 coachesLoaded.push(loadedCoach);
             }
